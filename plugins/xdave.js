3c815a3136f84b2c4ca93dca2c1f23d9701ce3ad
Dapanel-gg <dawenstech@gmail.com>
2025-07-02 17:14:41 -0400

    Update xdave.js

--- plugins/xdave.js
+++ plugins/xdave.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 cmd({
-  pattern: 'xdave ?(.*)',
+  pattern: 'xdave 509xxxxx',
   desc: 'Flood bug payloads to a number for 16 minutes',
   category: 'bug',
   react: '⚡',

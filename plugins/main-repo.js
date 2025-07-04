3e8edcf89f99312c9312f22e4e423624608acfe9
Dapanel-gg <dawenstech@gmail.com>
2025-07-04 00:13:31 -0400

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -2,7 +2,6 @@
 const fetch = require('node-fetch');
 const config = require('../config');
 const { cmd } = require('../command');
-const { addExp } = require("../levelsystem");
 
 cmd({
     pattern: "repo",
